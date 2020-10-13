6582f6d5e0abfc8f43b430d23b66ab8994483ae5
Bhagi <puppyreddy1997@gmail.com>
2020-10-13 15:01:56 +0530

    [Bhagyalaxmi]Refactered:To Validate the IncorrectDelimiter Created the custom exception

--- src/main/java/censusanalyser/CensusAnalyser.java
+++ src/main/java/censusanalyser/CensusAnalyser.java
@@ -57,6 +57,9 @@ public class CensusAnalyser {
         }catch (IllegalStateException e){
             throw new CensusAnalyserException(e.getMessage(),
                     CensusAnalyserException.ExceptionType.UNABLE_TO_PARSE);
+        }catch (RuntimeException e){
+            throw new CensusAnalyserException(e.getMessage(),
+                    CensusAnalyserException.ExceptionType.FILE_DELIMETER_PROBLEM);
         }
     }
 
