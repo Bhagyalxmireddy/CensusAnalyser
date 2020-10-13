2acca84cfa404ce38b3a37f1b6109908ee120250
Bhagi <puppyreddy1997@gmail.com>
2020-10-13 16:27:08 +0530

    [Bhagyalaxmi]Addede:validating the Testcase by creating the customexception

--- src/main/java/censusanalyser/CensusAnalyser.java
+++ src/main/java/censusanalyser/CensusAnalyser.java
@@ -21,14 +21,10 @@ public class CensusAnalyser {
             csvToBeanBuilder.withType(IndiaCensusCSV.class);
             csvToBeanBuilder.withIgnoreLeadingWhiteSpace(true);
             CsvToBean<IndiaCensusCSV> csvToBean = csvToBeanBuilder.build();
-
-            Iterator<IndiaCensusCSV> censusCSVIterator = csvToBean.iterator();;
-            int namOfEateries = 0;
-            while (censusCSVIterator.hasNext()) {
-                namOfEateries++;
-                IndiaCensusCSV censusData = censusCSVIterator.next();
-            }
-            return namOfEateries;
+            Iterator<IndiaCensusCSV>  censusCSVIterator = csvToBean.iterator();
+            Iterable<IndiaCensusCSV> csvIterable =() -> censusCSVIterator;
+            int numOfEnteries = (int) StreamSupport.stream(csvIterable.spliterator(),false).count();
+            return numOfEnteries;
         } catch (IOException e) {
             throw new CensusAnalyserException(e.getMessage(),
                     CensusAnalyserException.ExceptionType.CENSUS_FILE_PROBLEM);
