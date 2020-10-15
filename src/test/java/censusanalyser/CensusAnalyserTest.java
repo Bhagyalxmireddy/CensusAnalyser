ab19056e28b0513871e1c78081c16594d60f7b3a
Bhagi <puppyreddy1997@gmail.com>
2020-10-15 16:36:34 +0530

    [Bhagyalaxmi]Added:Added Test to sort the given census data by state

--- src/test/java/censusanalyser/CensusAnalyserTest.java
+++ src/test/java/censusanalyser/CensusAnalyserTest.java
@@ -1,5 +1,6 @@
 package censusanalyser;
 
+import com.google.gson.Gson;
 import org.junit.Assert;
 import org.junit.Test;
 import org.junit.rules.ExpectedException;
@@ -120,6 +121,17 @@ public class CensusAnalyserTest {
         } catch (CensusAnalyserException e) {
             Assert.assertEquals(CensusAnalyserException.ExceptionType.CENSUS_FILE_PROBLEM, e.type);
         }
+    }
+    @Test
+    public void givenIndiaCensusData_WhenSortedOnState_ShouldReturnSortedResult(){
+        try {
+            CensusAnalyser censusAnalyser = new CensusAnalyser();
+            censusAnalyser.loadIndiaCensusData(INDIA_CENSUS_CSV_FILE_PATH);
+            String sortedCensusData = censusAnalyser.getStateWiseSortedCensusData(INDIA_CENSUS_CSV_FILE_PATH);
+            IndiaCensusCSV[] censusCSV = new Gson().fromJson(sortedCensusData, IndiaCensusCSV[].class);
+            Assert.assertEquals("Uttar Pradesh", censusCSV[0].state);
+        }catch (CensusAnalyserException e){}
+
     }
 
   }
