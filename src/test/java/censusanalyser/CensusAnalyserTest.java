3d2711cc659d28e13f299da91ad37037b96b4991
Bhagi <puppyreddy1997@gmail.com>
2020-10-16 00:08:24 +0530

    [Bhagyalaxmi]Added:Added Test to Sort the given census Data by stateArea

--- src/test/java/censusanalyser/CensusAnalyserTest.java
+++ src/test/java/censusanalyser/CensusAnalyserTest.java
@@ -154,6 +154,16 @@ public class CensusAnalyserTest {
 
     }
     @Test
+    public void givenIndiaCensusData_WhenSortedOnArea_ShouldReturnSortedResult(){
+        try {
+            CensusAnalyser censusAnalyser = new CensusAnalyser();
+            censusAnalyser.loadIndiaCensusData(INDIA_CENSUS_CSV_FILE_PATH);
+           String areaSortedData = censusAnalyser.getStateCensusAreaSortedData();
+            IndiaCensusCSV[] censusCSVS = new Gson().fromJson(areaSortedData,IndiaCensusCSV[].class);
+            Assert.assertEquals(342239,censusCSVS[0].areaInSqKm);
+        }catch (CensusAnalyserException e){}
+    }
+    @Test
     public void givenIndiastateCode_WhenSortedOnState_ShouldReturnSortedResult(){
         try{
             CensusAnalyser censusAnalyser = new CensusAnalyser();
