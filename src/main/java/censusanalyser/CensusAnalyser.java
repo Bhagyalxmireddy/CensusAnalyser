1e1527edc8584787b2fe713979b536fb9ed75eda
Bhagi <puppyreddy1997@gmail.com>
2020-10-15 11:29:36 +0530

    [Bhagyalaxmi]Refactered:Refactered code to support list in place of iterator

--- src/main/java/censusanalyser/CensusAnalyser.java
+++ src/main/java/censusanalyser/CensusAnalyser.java
@@ -10,6 +10,7 @@ import java.nio.file.Files;
 import java.nio.file.Paths;
 import java.util.InputMismatchException;
 import java.util.Iterator;
+import java.util.List;
 import java.util.stream.StreamSupport;
 
 public class CensusAnalyser {
@@ -18,9 +19,8 @@ public class CensusAnalyser {
         try( Reader reader = Files.newBufferedReader(Paths.get(csvFilePath));)
         {
             ICSVBulider csvBulider = CSVBuliderFactory.createCSVBulider();
-            Iterator<IndiaCensusCSV>  censusCSVIterator = csvBulider.getCSVFileIterator(reader,IndiaCensusCSV.class);
-
-            return this.getCount(censusCSVIterator);
+            List<IndiaCensusCSV> censusCSVList = csvBulider.getCSVFileList(reader,IndiaCensusCSV.class);
+            return censusCSVList.size();
         } catch (IOException e) {
             throw new CensusAnalyserException(e.getMessage(),
                     CensusAnalyserException.ExceptionType.CENSUS_FILE_PROBLEM);
@@ -36,7 +36,6 @@ public class CensusAnalyser {
     public int loadIndianStateCode(String csvFilePath) throws CensusAnalyserException {
         try (Reader reader = Files.newBufferedReader(Paths.get(csvFilePath));) {
             ICSVBulider csvBulider = CSVBuliderFactory.createCSVBulider();
-
             Iterator<IndiaStateCodeCSV>  stateCSVFileIterator = csvBulider.getCSVFileIterator(reader,IndiaStateCodeCSV.class);
             return getCount(stateCSVFileIterator);
         } catch (IOException e) {
--- src/main/java/censusanalyser/ICSVBulider.java
+++ src/main/java/censusanalyser/ICSVBulider.java
@@ -2,12 +2,15 @@ package censusanalyser;
 
 import java.io.Reader;
 import java.util.Iterator;
+import java.util.List;
 
 public class ICSVBulider<E> {
     public  Iterator<E> getCSVFileIterator(Reader reader, Class csvClass) throws CSVBuliderException{
+        return  null;
+    }
+    public List<E> getCSVFileList(Reader reader, Class csvClass) throws CSVBuliderException{
         return null;
     }
-
 
 
 }
